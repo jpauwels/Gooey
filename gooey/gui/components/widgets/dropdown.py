badc178d67dedca9872dfd67f948c4de078a6a75
Johan Pauwels <jpauwels@users.noreply.github.com>
2020-09-11 16:35:55 +0100

    closes #541 - hide "Select Option" in dropdown if default is provided

--- gooey/gui/components/widgets/dropdown.py
+++ gooey/gui/components/widgets/dropdown.py
@@ -10,23 +10,29 @@ from gooey.gui.lang.i18n import _
 class Dropdown(TextContainer):
 
     def getWidget(self, parent, *args, **options):
-        default = _('select_option')
+        choices = [str(choice) for choice in self._meta['choices']]
+        if self._meta['default'] is None:
+            default = _('select_option')
+            choices = [str(default)] + choices
+        else:
+            default = self._meta['default']
         return wx.ComboBox(
             parent=parent,
             id=-1,
             # str conversion allows using stringyfiable values in addition to pure strings
             value=str(default),
-            choices=[str(default)] + [str(choice) for choice in self._meta['choices']],
+            choices=choices,
             style=wx.CB_DROPDOWN)
 
     def setOptions(self, options):
         with self.retainSelection():
             self.widget.Clear()
-            self.widget.SetItems([_('select_option')] + options)
+            select_option = [_('select_option')] if self._meta['default'] is None else []
+            self.widget.SetItems(select_option + options)
 
     def setValue(self, value):
         ## +1 to offset the default placeholder value
-        index = self._meta['choices'].index(value) + 1
+        index = self._meta['choices'].index(value) + 1 if self._meta['default'] is None else 0
         self.widget.SetSelection(index)
 
     def getWidgetValue(self):
